76f23bc66da93db4305fc3776de70da0876bf097
alfvin.ridwanto <alfvin.ridwanto@mbww.com>
2017-07-30 08:12:08 +0800

    audible notiication when server sends alert text to node

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 /* eslint-disable  func-names */
-/*eslint no-magic-numbers: ["error", { "ignore": [1000] }]*/
+/*eslint no-magic-numbers: ["error", { "ignore": [1000, 200] }]*/
 
 'use strict';
 
 var SmartObject = require('smartobject');
 var process = require('child_process');
+var request = require('request');
 var exec = process.exec;
+var lwm2mHost = "192.168.0.151";
+var lwm2mPort = 5683;
 // initialize Resources that follow IPSO definition
 var so = new SmartObject();
 
-// initialize your Resources
-// oid = 'temperature', iid = 0
 var deviceId = 0;
 so.init('status', deviceId, {
     'temperature': 0,
-    'time': new Date().getTime()
+    'time': new Date().getTime(),
+    'serverMessage': 'OK'
 });
 
+/** @this registered
+* @param {String} msg alert message
+* Send alert text to node-RED for audible notification
+* @returns {void}
+*/
+var sendAlert = function(msg) {
+  var url = "http://localhost:1880/alert";
+  request(
+    {
+      'method': 'POST',
+    'uri': url,
+    'body': {'text': msg},
+    'json': true,
+    }
+  , function (error, response, body) {
+      if (response.statusCode === 200) {
+        console.log('OK')
+      } else {
+        console.log('error: '+ response.statusCode)
+        console.log(body)
+      }
+    }
+  );
+}
 
 var getCpuTemp = function () {
   exec("cat /sys/class/thermal/thermal_zone0/temp", function returnTemp (error, stdout, stderr) {
+    var msg = so.get('status', deviceId, 'serverMessage');
+    console.log(msg);
+    if (msg !== 'OK') {
+      sendAlert(msg);
+    }
     if (error === null) {
-      // You must send time (X axis) and a temperature value (Y axis)
-      var date = new Date().getTime();
       var temp = parseFloat(stdout)/1000;
 
-      // var so2 = cnode.getSmartObject();
       so.write('status', deviceId, 'temperature', temp, function (err, data) {
         if (err) {
           console.log(err);
@@ -38,10 +66,8 @@ var getCpuTemp = function () {
         }
           console.log(data);
       });
-      console.log('output: ' + date + ":" + temp);
-      // socket.emit('temperatureUpdate', date, temp);
+      console.log('output: ' + temp);
     } else {
-      console.log('exec error: ' + error);
       console.log(stderr);
     }
   })
@@ -52,7 +78,7 @@ var getCpuTemp = function () {
 */
 var registered = function() {
   var sobject = this.getSmartObject();
-  var timeInterval = 5000;
+  var timeInterval = 8000;
   console.log(sobject);
   setInterval(getCpuTemp, timeInterval);
 };
@@ -66,11 +92,10 @@ var onReconnect = function() {
 }
 
 var CoapNode = require('coap-node');
+// Get mac address for unique ID
 exec("cat /sys/class/net/eth0/address", function (error, stdout, stderr) {
   if (error === null) {
-    console.log(stdout);
-    var nodeName = "";
-    nodeName = String(stdout);
+    var nodeName = String(stdout);
     // Instantiate a machine node with a client name and your smart object
     var cnode = new CoapNode(nodeName, so);
 
@@ -81,8 +106,7 @@ exec("cat /sys/class/net/eth0/address", function (error, stdout, stderr) {
     cnode.on('reconnect', onReconnect);
 
     // register to a Server with its ip and port
-    var port = 5683;
-    cnode.register('192.168.0.151', port, function register (err, rsp) {
+    cnode.register(lwm2mHost, lwm2mPort, function register (err, rsp) {
         console.log(rsp);      // { status: '2.05' }
         if (err) {
           console.log(err);
